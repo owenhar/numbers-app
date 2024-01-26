bf5b742aa28c457af8dd4ed4e7ff5473754a2d8b
Owen Harris <harriowe@iu.edu>
2024-01-25 19:37:14 -0500

    Changes to call API/service instead of doing the math

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-document.getElementById("submit").onclick = function() {
-    let a = document.getElementById("a")
-    let b = document.getElementById("b")
-    let out = document.getElementById("out")
-    let res = Number(a.value) + Number(b.value)
+document.getElementById("submit").onclick = async function() {
+    let a = document.getElementById("a");
+    let b = document.getElementById("b");
+    let out = document.getElementById("out");
+    let n = a.value;
+    let m = b.value;
+    const response = await fetch(`http://localhost:3000/add/${n}/${m}`);
+    const res = await response.json();
+    
     console.log(res) // Simple debugging
     out.innerHTML = res
 }
