71af52edca555bbbb7c350c0cf7db3256db3a8f8
Owen Harris <harriowe@iu.edu>
2024-01-25 19:38:59 -0500

    Switched to old style

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
-document.getElementById("submit").onclick = async function() {
-    let a = document.getElementById("a");
-    let b = document.getElementById("b");
-    let out = document.getElementById("out");
-    let n = a.value;
-    let m = b.value;
-    const response = await fetch(`http://localhost:3000/add/${n}/${m}`);
-    const res = await response.json();
-    
+document.getElementById("submit").onclick = function() {
+    let a = document.getElementById("a")
+    let b = document.getElementById("b")
+    let out = document.getElementById("out")
+    let res = Number(a.value) + Number(b.value)
     console.log(res) // Simple debugging
     out.innerHTML = res
 }
