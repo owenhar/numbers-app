61ef5c5ecedf5fc9fcc5f60767789dad7a3a55a0
Owen Harris <harriowe@iu.edu>
2024-02-08 19:33:38 -0500

    added operations

--- index.js
+++ index.js
@@ -11,3 +11,42 @@ document.getElementById("submit").onclick = async function() {
     console.log(res) // Simple debugging
     out.innerHTML = res
 }
+document.getElementById("submit-").onclick = async function() {
+    let a = document.getElementById("a-");
+    let b = document.getElementById("b-");
+    let out = document.getElementById("out-");
+    let n = a.value;
+    let m = b.value;
+    const response = await fetch(`https://arithmetic-service-harriowe.onrender.com/subtract/${n}/${m}`);
+    // console.log(response);
+    const res = await response.json();
+    
+    console.log(res) // Simple debugging
+    out.innerHTML = res
+}
+document.getElementById("submit*").onclick = async function() {
+    let a = document.getElementById("a*");
+    let b = document.getElementById("b*");
+    let out = document.getElementById("out*");
+    let n = a.value;
+    let m = b.value;
+    const response = await fetch(`https://arithmetic-service-harriowe.onrender.com/multiply/${n}/${m}`);
+    // console.log(response);
+    const res = await response.json();
+    
+    console.log(res) // Simple debugging
+    out.innerHTML = res
+}
+document.getElementById("submit/").onclick = async function() {
+    let a = document.getElementById("a/");
+    let b = document.getElementById("b/");
+    let out = document.getElementById("out/");
+    let n = a.value;
+    let m = b.value;
+    const response = await fetch(`https://arithmetic-service-harriowe.onrender.com/divide/${n}/${m}`);
+    // console.log(response);
+    const res = await response.json();
+    
+    console.log(res) // Simple debugging
+    out.innerHTML = res
+}
