da302107d8261526dc11d8fc698e4c88b3e77fee
Owen Harris <harriowe@iu.edu>
2024-02-22 19:46:20 -0500

    added primality check

--- index.js
+++ index.js
@@ -50,3 +50,12 @@ document.getElementById("submit/").onclick = async function() {
     console.log(res) // Simple debugging
     out.innerHTML = res
 }
+
+async function isPrime() {
+    const numberToTest = document.getElementById("numberToTest");            
+    let n = numberToTest.value;            
+    const response = await fetch("https://primes-service-latest-ik1x.onrender.com/primes/" + n);
+    const result = await response.json();
+    document.getElementById("primeCheckResult").innerText = result;
+}
+
